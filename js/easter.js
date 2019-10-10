3f7f03b5bd5e7906286000b0e172138db1ab26d7
Théo <champion.theo.p@gmail.com>
2019-10-10 23:23:28 +0200

    Added a new in index.html

---
+++ js/easter.js
@@ -0,0 +1,24 @@
+// on crée une variable array qui contiendra toutes les entrées au clavier et une variable now qui compte combien de touches ont étés rentrées.
+var code = new Array();
+var now = 0;
+
+// konami code
+// ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft", "ArrowRight","ArrowLeft", "ArrowRight", "b", "a"]
+
+// on prend la fenetre et on fait un event avec une fonction anonyme
+var doc = document
+doc.addEventListener("keydown", function(evt){
+	code[now] = evt.key;
+	now++;
+
+	if(now == 10){
+		var trueCode = ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft", "ArrowRight","ArrowLeft", "ArrowRight", "b", "a"].toString();
+		if(code.toString() == trueCode){
+			easter();
+		}
+	}
+});
+
+function easter(){
+	close();
+}
